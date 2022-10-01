bb25d761de46d526ed87f1e5a7089d3c61170134
Subham Ranjan <subhamranjanhalo@gmail.com>
2022-10-01 06:02:03 +0530

    Bean Class Added

---
+++ Crime dao/Crime Management/bean/Criminal.java
@@ -0,0 +1,82 @@
+package Bean;
+
+public class Criminal {
+
+	private String name;
+	private int age;
+	private String gender;
+	private String mark;
+	private String address;
+	private int CriminalFirstArrested;
+	
+	public Criminal(){
+	}
+
+	public Criminal(String name, int age, String gender, String mark, String address, int criminalFirstArrested) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.mark = mark;
+		this.address = address;
+		CriminalFirstArrested = criminalFirstArrested;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getCriminalFirstArrested() {
+		return CriminalFirstArrested;
+	}
+
+	public void setCriminalFirstArrested(int criminalFirstArrested) {
+		CriminalFirstArrested = criminalFirstArrested;
+	}
+
+	@Override
+	public String toString() {
+		return "Criminal [name=" + name + ", age=" + age + ", gender=" + gender + ", mark=" + mark + ", address="
+				+ address + ", CriminalFirstArrested=" + CriminalFirstArrested + "]";
+	}
+	
+	
+	
+	
+}
