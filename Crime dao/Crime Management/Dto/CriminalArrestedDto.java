ce0f54a76f371ebe7e1f49be602fab0aa5445cce
Subham Ranjan <subhamranjanhalo@gmail.com>
2022-10-01 07:45:30 +0530

    Adding Data Transfer Object Dto classess

---
+++ Crime dao/Crime Management/Dto/CriminalArrestedDto.java
@@ -0,0 +1,67 @@
+package Dto;
+
+public class CriminalArrestedDto {
+	private String name;
+	private int age;
+	private String gender;
+	private String mark;
+	private String address;
+	private String policeStationname;
+	
+	public CriminalArrestedDto() {
+		
+	}
+	@Override
+	public String toString() {
+		return "CriminalArrestedDto [name=" + name + ", age=" + age + ", gender=" + gender + ", mark=" + mark
+				+ ", address=" + address + ", policeStationname=" + policeStationname + "]";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMark() {
+		return mark;
+	}
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPoliceStationname() {
+		return policeStationname;
+	}
+	public void setPoliceStationname(String policeStationname) {
+		this.policeStationname = policeStationname;
+	}
+	public CriminalArrestedDto(String name, int age, String gender, String mark, String address,
+			String policeStationname) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.mark = mark;
+		this.address = address;
+		this.policeStationname = policeStationname;
+	}
+	
+	
+}
---
+++ Crime dao/Crime Management/Dto/CriminalsCrimeDto.java
@@ -0,0 +1,82 @@
+package Dto;
+
+public class CriminalsCrimeDto {
+	private String criminalName;
+	private int age;
+	private String gender;
+	private String mark;
+	private String address;
+	private String description;
+	
+	@Override
+	public String toString() {
+		return "CriminalsCrimeDto [criminalName=" + criminalName + ", age=" + age + ", gender=" + gender + ", mark="
+				+ mark + ", address=" + address + ", description=" + description + "]";
+	}
+
+	public String getCriminalName() {
+		return criminalName;
+	}
+
+	public void setCriminalName(String criminalName) {
+		this.criminalName = criminalName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public CriminalsCrimeDto() {
+		
+	}
+	
+	public CriminalsCrimeDto(String criminalName, int age, String gender, String mark, String address,
+			String description) {
+		super();
+		this.criminalName = criminalName;
+		this.age = age;
+		this.gender = gender;
+		this.mark = mark;
+		this.address = address;
+		this.description = description;
+	}
+	
+	
+	
+}
