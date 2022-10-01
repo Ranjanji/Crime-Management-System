dd90b5b9976acedb4c697cefca33ee9034bc829d
Subham Ranjan <subhamranjanhalo@gmail.com>
2022-10-01 06:34:10 +0530

    Exceptions Class Added

---
+++ Crime dao/Crime Management/exceptions/CrimeExceptions.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+public class CrimeExceptions extends Exception{
+	public CrimeExceptions() {
+		// TODO Auto-generated constructor stub
+	}
+	public CrimeExceptions(String m) {
+		super(m);
+	}
+}
---
+++ Crime dao/Crime Management/exceptions/CriminalExceptions.java
@@ -0,0 +1,11 @@
+package Exceptions;
+
+public class CriminalExceptions extends Exception{
+	
+	public CriminalExceptions() {
+		// TODO Auto-generated constructor stub
+	}
+	public CriminalExceptions(String m) {
+		super(m);
+	}
+}
